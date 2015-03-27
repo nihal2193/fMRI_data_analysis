d2472887fe9d3adf74234c6c560e157f3baf3183
Saloni <salonitandon2008@gmail.com>
2015-03-27 22:03:58 +0530

    max_corr.R, to find topmost coordinates and timeseries of those top most coordinates

---
+++ max_corr.R
@@ -0,0 +1,45 @@
+#source('../functions/ts_xyz.R')
+args <- commandArgs(trailingOnly = TRUE)
+f_name <- args[1]
+f2_name <- args[2]
+library('AnalyzeFMRI')
+print('library loaded')
+
+first <- 1
+last  <- 150
+nvolumes <- 1342
+#ts_aud_lang <- array(dim=c((last - first + 1), nvolumes))
+raw_corr <- f.read.volume(f_name)
+raw_corr_des <- sort(raw_corr,decreasing = TRUE)
+#index_sorted <- matrix(0, ncol=5, nrow=50)
+index_sorted <- NULL
+coords <- which(raw_corr==raw_corr_des[1], arr.ind = TRUE, useNames = FALSE)
+#ts_aud_lang[first,] <- ts_xyz(coords[1:3],'../run2/')
+index_sorted <- matrix(c(coords,raw_corr_des[1]),nrow=1,ncol=5)
+ts_1 <- f.read.nifti.ts(f2_name,coords[1],coords[2],coords[3]) 
+time_series <- matrix(ts_1,nrow=1,ncol=1342)
+mask <- array(dim = c(64,64,30,1))
+mask[coords] <- raw_corr_des[first]
+print(raw_corr_des[first])
+print(raw_corr_des[last])
+#time_series <- array(dim=c(150,1342))
+for (i in 2:last)
+{
+coords <- which(raw_corr==raw_corr_des[i], arr.ind = TRUE, useNames = FALSE)
+time_series <- rbind(time_series,f.read.nifti.ts(f2_name,coords[1],coords[2],coords[3]))
+index_sorted <- rbind(index_sorted,c(coords,raw_corr_des[i]))
+mask[coords] <- raw_corr_des[i]
+#ts_aud_lang[i,] <- ts_xyz(coords[1:3],'../run2/')
+}
+
+mask[is.na(mask)] = 0
+
+f.write.nifti(mask, file='max_coords_mask', size='float', nii=FALSE)
+t1 <- t(index_sorted)
+write(t1, file = 'index_sorted', ncolumns = 5)
+time_series[is.na(time_series)] = 0
+tst <- t(time_series)
+write(tst, file='time_series_150_points.csv',sep=',',ncolumns=1342)
+#write(ts_aud_lang, file = 'aud_lang.ts', ncolumns = nvolumes)
+
+
